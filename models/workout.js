818e2a29ef9021b565f102a186bc497dfdc56d01
kelseck <kelseyeckelberry@gmail.com>
2021-01-23 20:50:29 -0600

    workout schema

--- models/workout.js
+++ models/workout.js
@@ -3,19 +3,39 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Workout Name"
-  },
-  value: {
-    type: Number,
-    required: "Workout Time"
-  },
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Exercise Type",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Workout Name",
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
